a7394ce2b36404c99fd9cefa0fffab37e48b7c64
YoungseoChoi23 <134666006+YoungseoChoi23@users.noreply.github.com>
2023-05-29 02:07:16 +0900

    Update ETTTP_Server_skeleton.py

--- ETTTP_Server_skeleton.py
+++ ETTTP_Server_skeleton.py
@@ -53,13 +53,31 @@ if __name__ == '__main__':
             if stext!=("ACK start is client"):
                 print("비정상 종료")
                 break
-        ###################################################################
+            else:
+               print("ACK 정상 수신")
+                
+            AckCheckText = client_socket.recv(SIZE).decode()
+            if AckCheckText != ("ACK"):
+                print("ACK를 수신하지 못함")
+            else:
+                root = TTT(client=False,target_socket=client_socket, src_addr=MY_IP,dst_addr=client_addr[0])
+                root.play(start_user=start)
+                root.mainloop()
         
+                client_socket.close()
+        
+                break
+                server_socket.close()
+
+                
         root = TTT(client=False,target_socket=client_socket, src_addr=MY_IP,dst_addr=client_addr[0])
         root.play(start_user=start)
         root.mainloop()
-        
         client_socket.close()
         
         break
-    server_socket.close()
+        server_socket.close()
+
+              
+        ###################################################################
+ 
