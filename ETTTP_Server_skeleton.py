515382c2d2dd2e1c76291d15e0e6789773dc2a3a
YoungseoChoi23 <134666006+YoungseoChoi23@users.noreply.github.com>
2023-06-02 17:55:34 +0900

    Update ETTTP_Server_skeleton.py

--- ETTTP_Server_skeleton.py
+++ ETTTP_Server_skeleton.py
@@ -31,6 +31,7 @@ if __name__ == '__main__':
     while True:
         def remove_substring(string, start, end):
             return string[:start] + string[end+1:]
+        #ETTTP형식의 메시지인지 확인하기 위해 substring을 추출하는 함수를 새로 생성
         client_socket, client_addr = server_socket.accept()
         print("클라이언트의 주소는 "+str(client_addr)+"입니다. ")
         start = random.randrange(0,2)
@@ -42,40 +43,46 @@ if __name__ == '__main__':
         
         if start==0:
           #서버 먼저 시작.
-          #client에게 server가 시작한다는 message를 전송.
+          #server가 client에게 시작한다는 message를 전송.
             client_socket.send(bytes("SEND ETTTP/1.0\r\n"
             +"Host:"+client_addr[0]+"\r\n"
             +"First-Move: ME\r\n\r\n" ,"utf-8"))
             print("start is server")
             # Receive ack - if ack is correct, start game
             stext=client_socket.recv(SIZE).decode()
-            #0. 메세지를 첫 띄어쓰기 나올 때 이후만 떼어씀
+            #client가 보낸 ACK message를 받아서 stext에 넣음.
+            #0. ETTTP message에서, ACK string 이후의 string만 추출하기 위해 index를 확인. 
             start_index = stext.find("A")
             end_index = stext.find(" ")
-            #1. 메세지를 띄어쓰기 후까지만 활용
+            #1. ACK string 이후의 string만 활용
             stext = remove_substring(stext, start_index, end_index)
             stext_list=stext.split("\r\n")
-            if (stext_list[0]!=("ETTTP/1.0"))or(stext_list[1]!="Host:"+MY_IP):#ETTTP형식에 맞지 않으면. 까봐서 나한테 온 건지 확인해야하니까 MY_IP
+            if (stext_list[0]!=("ETTTP/1.0"))or(stext_list[1]!="Host:"+MY_IP):
+              #1) ETTTP 형식에 맞는지 확인 or 2) 내 IP주소와 받은 message에서의 IP주소가 같은지 확인.
                 print("비정상 종료")
                 client_socket.close()
                 break          
             else: 
                 print("ACK 정상 수신")
             
-        else: #클라이언트 먼저 시작.
+        else: 
+          #클라이언트 먼저 시작.
+          #server가 client에게 시작한다는 message를 전송.
             client_socket.send(bytes("SEND ETTTP/1.0\r\n"
             +"Host:"+client_addr[0]+"\r\n"
             +"First-Move: YOU\r\n\r\n" ,"utf-8"))
             print("start is client")
             # Receive ack - if ack is correct, start game
             stext=client_socket.recv(SIZE).decode()
-            #0. 메세지를 첫 띄어쓰기 나올 때 이후만 떼어씀
+            #client가 보낸 ACK message를 받아서 stext에 넣음.
+            #0. ETTTP message에서, ACK string 이후의 string만 추출하기 위해 index를 확인. 
             start_index = stext.find("A")
             end_index = stext.find(" ")
-            #1. 메세지를 띄어쓰기 후까지만 활용
+            #1. ACK string 이후의 string만 활용
             stext = remove_substring(stext, start_index, end_index)
             stext_list=stext.split("\r\n")
-            if (stext_list[0]!=("ETTTP/1.0"))or(stext_list[1]!="Host:"+MY_IP):#ETTTP형식에 맞지 않으면
+            if (stext_list[0]!=("ETTTP/1.0"))or(stext_list[1]!="Host:"+MY_IP):
+              #1) ETTTP 형식에 맞는지 확인 or 2) 내 IP주소와 받은 message에서의 IP주소가 같은지 확인.
                 print("비정상 종료")
                 client_socket.close()
                 break
@@ -83,7 +90,7 @@ if __name__ == '__main__':
                 print("ACK 정상 수신")
         ###################################################################
 
-                
+        # Start game        
         root = TTT(client=False,target_socket=client_socket, src_addr=MY_IP,dst_addr=client_addr[0])
         root.play(start_user=start)
         root.mainloop()
