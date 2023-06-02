6d11a440e7b5b41c64a59d1044edbcf60b3a7dea
YoungseoChoi23 <134666006+YoungseoChoi23@users.noreply.github.com>
2023-06-02 17:48:14 +0900

    Update ETTTP_Client_skeleton.py

--- ETTTP_Client_skeleton.py
+++ ETTTP_Client_skeleton.py
@@ -31,23 +31,27 @@ if __name__ == '__main__':
     with socket(AF_INET, SOCK_STREAM) as client_socket:
         def remove_substring(string, start, end):
             return string[:start] + string[end+1:]
+        #ETTTP형식의 메시지인지 확인하기 위해 substring을 추출하는 함수를 새로 생성
         client_socket.connect(SERVER_ADDR)  
         print("연결되었습니다.")
         ###################################################################
         # Receive who will start first from the server
         ######################### Fill Out ################################
-        ctext=client_socket.recv(SIZE).decode()#server가 보낸 메세지 까본다.
-        #0. 메세지를 첫 띄어쓰기 나올 때 이후만 떼어씀
+        ctext=client_socket.recv(SIZE).decode()
+        #server가 보낸 message를 받아서 ctext에 넣음.
+        #0. ETTTP message에서, SEND/ACK string 이후의 string만 추출하기 위해 index를 확인. 
         start_index = ctext.find("S")
         end_index = ctext.find(" ")
-        #1. 메세지를 띄어쓰기 후까지만 활용
+        #1. SEND/ACK string 이후의 string만 활용
         ctext = remove_substring(ctext, start_index, end_index)
         ctext_list=ctext.split("\r\n")
-        if (ctext_list[0]!=("ETTTP/1.0"))or(ctext_list[1]!="Host:"+MY_IP):#1)ETTTP형식에 맞지 않으면. 2)나한테 온 건지 까봐야 함.
+        if (ctext_list[0]!=("ETTTP/1.0"))or(ctext_list[1]!="Host:"+MY_IP):
+          #1) ETTTP 형식에 맞는지 확인 or 2) 내 IP주소와 받은 message에서의 IP주소가 같은지 확인.
             print("비정상 종료")
             client_socket.close()
             exit()
         if ctext_list[2]=="First-Move: ME":
+            # 만약 첫 시작이 server라면
             start=0
             print("start is server")
             # Send ACK 
@@ -55,6 +59,7 @@ if __name__ == '__main__':
             +"Host:"+SERVER_IP+"\r\n"
             +"First-Move: ME\r\n\r\n" ,"utf-8"))
         else :
+          # 만약 첫 시작이 client라면
             start=1
             print("start is client")
             # Send ACK 
